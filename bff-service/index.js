eea7f80aea410f345717d523b4346a23d789367d
Dzmitry Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2020-12-13 23:33:26 +0300

    refactor: before deploy on EBS

--- bff-service/index.js
+++ bff-service/index.js
@@ -10,10 +10,10 @@ const cacheConfig = {
         path: /products$/i,
         url: `${process.env.PRODUCTSERVICE}/products`
 }
-// console.log(process.env);
 
 const cache = duration => {
     return async (req, res, next) => {
+        console.log('in Cache function')
         if (cacheConfig.methods.test(req.method) && cacheConfig.path.test(req.url)) {
             let key = `${req.method}_${req.url}`;
             const cached = mcache.get(key);
@@ -39,12 +39,19 @@ const cache = duration => {
     }
 }
 
+const validator = (req, res, next) => {
+    const result = Object.keys(env.parsed).find(key => { return (new RegExp(`/${key}/`, 'i')).test(req.originalUrl) })
+    result ? next() : res.status(502).send('Cannot process request');
+}
+
 const app = express();
 
 app.use(morgan('dev'));
+app.use(validator);
 app.use(cache(120));
 
 Object.entries(env.parsed).forEach(([key, value]) => {
+    console.log('KEY: [%o], VALUE: [%O]', key, value)
     app.use(`/${key.toLowerCase()}`, createProxyMiddleware({
         target: value,
         changeOrigin: true,
@@ -53,10 +60,10 @@ Object.entries(env.parsed).forEach(([key, value]) => {
             [`^/${key.toLowerCase()}`]: '',
         },
         onProxyRes: async (proxyRes, req, res) => {
-            // console.log('PROXY_RES: [%o]', 'proxyRes');
+            console.log('PROXY_RES: [%o]', 'proxyRes');
         },
         onProxyReq: (proxyReq, req, res) => {
-            // console.log('PROXY_REQ: [%o]', 'proxyReq');
+            console.log('PROXY_REQ: [%o]', 'proxyReq');
         }
     }));
 })
@@ -65,4 +72,7 @@ app.get('/info', (req, res, next) => {
     res.send('This is a PROXY, baby.')
 });
 
+console.log(PROXY_HOST)
+console.log(PROXY_PORT)
+console.log(env)
 app.listen(PROXY_PORT, PROXY_HOST);
