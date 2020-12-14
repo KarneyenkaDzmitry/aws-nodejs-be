dd540a738e7aafe67014958328a857124db09be1
Dzmitry Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2020-12-14 12:40:36 +0300

    refactor: last update before Nest.js implementation

--- bff-service/index.js
+++ bff-service/index.js
@@ -4,38 +4,44 @@ const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const axios = require('axios');
 const mcache = require('memory-cache');
-const { PORT = 8080} = process.env;
+const { PORT = 8080 } = process.env;
 const cacheConfig = {
-        methods: /(GET|OPTIONS)/i,
-        path: /products$/i,
-        url: `${process.env.PRODUCTSERVICE}/products`
+    methods: /(GET|OPTIONS)/i,
+    path: /products$/i,
+    url: `${process.env.PRODUCTSERVICE}/products`
 }
 
 const cache = duration => {
     return async (req, res, next) => {
-        console.log('in Cache function')
-        if (cacheConfig.methods.test(req.method) && cacheConfig.path.test(req.url)) {
-            let key = `${req.method}_${req.url}`;
-            const cached = mcache.get(key);
-            if (cached) {
-                console.log('Cache have been leveraged!')
-                res.set(cached.headers).status(cached.status).send(cached.data)
-                return
-            } else {
-                const response = await axios({
-                    method: req.method,
-                    url: cacheConfig.url,
-                })
-                const store = {
-                    headers: response.headers,
-                    data: response.data,
-                    status: response.status
+        try {
+            if (cacheConfig.methods.test(req.method) && cacheConfig.path.test(req.url)) {
+                let key = `${req.method}_${req.url}`;
+                const cached = mcache.get(key);
+                if (cached) {
+                    console.log('Cache have been leveraged!')
+                    res.set(cached.headers).status(cached.status).send(cached.data)
+                    return
+                } else {
+                    const response = await axios({
+                        method: req.method,
+                        url: cacheConfig.url,
+                    })
+                    const { status, headers, data } = response;
+                    if (status === 200) {
+                        const store = { headers, data, status };
+                        const result = mcache.put(key, store, duration * 1000);
+                        console.log('The following data have just been Cached. \nKEY: |%o|\nVALUE:\n|%o|', key, JSON.stringify(result));
+                    } else {
+                        res.set(headers).status(status).send(data);
+                    }
                 }
-                const result = mcache.put(key, store, duration * 1000);
-                console.log('The following data have just been Cached. \nKEY: |%o|\nVALUE:\n|%o|', key, JSON.stringify(result));
             }
-        } 
+        } catch (error) {
+            console.log('Error in Cache middleware: [%o]', JSON.stringify(error));
+            res.set({ 'Access-Control-Allow-Origin': '*' }).status(500).send(error.message);
+        } finally {
             next();
+        }
     }
 }
 
